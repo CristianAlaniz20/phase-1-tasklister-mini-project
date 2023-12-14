e8d02fdb82b6bbdda1c5240917c481261ee227d1
Cristian Alaniz Benitez <alaniz21cris@gmail.com>
2023-12-13 20:48:56 -0800

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const toDo = e.target.querySelector("#new-task-description").value
+    buildToDo(toDo)
+    }
+  );
+
 });
+
+function buildToDo(x) {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    li.textContent = x;
+    btn.textContent = "x";
+    document.querySelector("#tasks").appendChild(li);
+    li.appendChild(btn);
+
+    btn.addEventListener("click", () => li.remove())
+}
